02dd2cf3463200e16fe908a1ed9773ee9c73d0ba
Hope Inman <inmanhk@mail.uc.edu>
2026-02-24 01:34:53 +0000

    update

--- script.js
+++ script.js
@@ -3,13 +3,37 @@ const form = document.getElementById('checkInForm');
 const nameInput = document.getElementById('attendeeName');
 const  teamselect = document.getElementById('teamSelect');
 
+// Track Attendance
+let count = 0;
+const maxCount = 50;
+
+
 // Form Submission
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function(event) {
   e.preventDefault();
 
   // Get values
   const name = nameInput.value;
   const team = teamselect.value;
-console.log(name, team);
+  const teamName = teamselect.selectedOptions[0].text;
+
+console.log(name, teamName);
+
+// Increment Count
+count++;
+console.log("Total check-ins: ", count);
+
+// Update progress bar
+const percentage = Math.round((count / maxCount) * 100) + "%";
+console.log('Progress: ${percentage}');
+
+// Update Team Counter
+const teamCounter = document.getElementById(team + "Count");
+const current = parseInt(teamCounter.textContext);
+console.log("Previous team count: ", current);
+
+const newTotal = current + 1
+console.log("New team count: ", newTotal);
+
 
 });
